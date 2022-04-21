54fa88e7235cc0b5f05b3fe5d60fdbbc6d4acd98
Asrordin <ganijonovasroriddin84@gmail.com>
2022-04-21 11:54:39 +0500

    o'zgarish

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const elHeaderNav = document.querySelector('.site-nav')
-const elHeaderBtn = document.querySelector('.js-header-btn')
-
-// site-header__nav--open
-
-elHeaderBtn.addEventListener('click', () => {
-  elHeaderNav.classList.toggle('site-header__nav--open')
-}) 
